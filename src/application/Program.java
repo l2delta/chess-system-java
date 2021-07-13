6a3e66f5a5ea7c7f3aa540d2d82d8222dd06dc73
l2delta <luizlipes23@gmail.com>
2021-07-13 15:59:14 -0300

    First class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Position pos = new Position(3, 5);
 		
+		System.out.println(pos);
 		
-		System.out.println("hello world");
 
+		
 	}
 
 }
